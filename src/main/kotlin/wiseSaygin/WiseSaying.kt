5a637ad30c32b577a03ab97de05ac957bdc29eb0
HanJeongmok <wjdahe1845@gmail.com>
2026-04-30 15:14:28 +0900

    13

--- src/main/kotlin/wiseSaygin/WiseSaying.kt
+++ src/main/kotlin/wiseSaygin/WiseSaying.kt
@@ -1,12 +1,15 @@
 package wiseSaygin
 
 data class WiseSaying(
-    val id: Int,
+    var id: Int=0,
     var content: String,
     var author: String
 ){
     fun modify(content:String,  author: String){
         this.content=content
         this.author=author
+    }
+    fun isNew(): Boolean {
+        return id == 0
     }
 }
