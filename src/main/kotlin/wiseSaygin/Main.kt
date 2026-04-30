f197341a8ae5cb70c0f0392eb58be53376524859
HanJeongmok <wjdahe1845@gmail.com>
2026-04-30 11:17:07 +0900

    ch3, ch4

--- src/main/kotlin/wiseSaygin/Main.kt
+++ src/main/kotlin/wiseSaygin/Main.kt
@@ -1,21 +1,27 @@
 package wiseSaygin
 
 fun main() {
+
+    var lastId = 0
     println("== 명언 앱 ==")
-    while(true){
+    while (true) {
         print("명령) ")
 
         val input = readln()
-        when(input){
-            "종료"->{
+        when (input) {
+            "종료" -> {
                 println("프로그램을 종료합니다.")
                 break;
             }
-            "등록"->{
+
+            "등록" -> {
                 print("명언 : ")
-                val content= readln()
+                val content = readln()
                 print("작가 : ")
                 val author = readln()
+                val id = ++lastId
+
+                println("${id}번 명언이 등록되었습니다.")
             }
 
         }
